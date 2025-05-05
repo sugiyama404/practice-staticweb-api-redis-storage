0ea7353934c8cdee3839b68f67641ae21375d457
sugiyama404 <mmingriha@gmail.com>
2025-05-05 21:34:15 +0900

    Add static web app resources and configurations; implement system-assigned managed identity and define necessary variables

---
+++ infra/modules/static_web_app/azapi_resource.tf
@@ -0,0 +1,63 @@
+# Static Web App (azapi リソースを使用して実装)
+resource "azapi_resource" "static_web_app" {
+  type      = "Microsoft.Web/staticSites@2022-03-01"
+  name      = "securedemo-swa"
+  location  = var.resource_group.location
+  parent_id = var.resource_group.id
+
+  body = jsonencode({
+    properties = {
+      stagingEnvironmentPolicy = "Enabled"
+      allowConfigFileUpdates   = true
+      enterpriseGradeCdnStatus = "Disabled"
+      provider                 = "Custom"
+    }
+    sku = {
+      name = "Standard"
+      tier = "Standard"
+    }
+  })
+
+  tags = {
+    environment = "dev"
+    purpose     = "secure-webapp"
+  }
+}
+
+# Static Web App のバックエンド API として App Service を設定
+resource "azapi_resource" "static_web_app_linked_backend" {
+  type      = "Microsoft.Web/staticSites/linkedBackends@2022-03-01"
+  name      = "backend1"
+  parent_id = azapi_resource.static_web_app.id
+
+  body = jsonencode({
+    properties = {
+      backendResourceId = azurerm_linux_web_app.api.id
+      region            = azurerm_resource_group.rg.location
+    }
+  })
+
+  depends_on = [
+    azurerm_linux_web_app.api
+  ]
+}
+
+# Static Web App の アプリ設定
+resource "azapi_resource" "static_web_app_config" {
+  type      = "Microsoft.Web/staticSites/config@2022-03-01"
+  name      = "appsettings"
+  parent_id = azapi_resource.static_web_app.id
+
+  body = jsonencode({
+    properties = {
+      "REDIS_CONNECTION_STRING"   = "@Microsoft.KeyVault(SecretUri=https://${azurerm_key_vault.kv.name}.vault.azure.net/secrets/RedisConnectionString/)",
+      "STORAGE_CONNECTION_STRING" = "@Microsoft.KeyVault(SecretUri=https://${azurerm_key_vault.kv.name}.vault.azure.net/secrets/StorageConnectionString/)",
+      "API_URL"                   = "https://${azurerm_linux_web_app.api.default_hostname}/api"
+    }
+  })
+
+  depends_on = [
+    azurerm_linux_web_app.api,
+    azurerm_key_vault.kv
+  ]
+}
---
+++ infra/modules/static_web_app/variables.tf
@@ -0,0 +1,2 @@
+variable "resource_group" {}
+variable "subnet_app_subnet_id" {}
