13a9a99f8e6d83d56f16971a1a08edb54772d5aa
sugiyama404 <mmingriha@gmail.com>
2025-05-10 15:01:28 +0900

    Remove detailed descriptions for resource_group and subnet_app_subnet_id variables in static web app module

--- infra/modules/static_web_app/variables.tf
+++ infra/modules/static_web_app/variables.tf
@@ -1,19 +1,7 @@
-variable "resource_group" {
-  description = "The resource group where the static web app will be deployed"
-  type = object({
-    id       = string
-    location = string
-    name     = string
-  })
-}
-
+variable "resource_group" {}
+variable "subnet_app_subnet_id" {}
 variable "static_web_app_name" {
   description = "Name of the static web app"
   type        = string
   default     = "securedemo-swa"
-}
-
-variable "subnet_app_subnet_id" {
-  description = "The subnet ID for App Service integration"
-  type        = string
 }
