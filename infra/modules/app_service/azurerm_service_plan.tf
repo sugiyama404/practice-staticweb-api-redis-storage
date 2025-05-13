42aa34e17151ae4a8b1971dc1c2ec6878cf66894
sugiyama404 <mmingriha@gmail.com>
2025-05-13 21:00:03 +0900

    fix: Update App Service Plan name and location to use dynamic values

--- infra/modules/app_service/azurerm_service_plan.tf
+++ infra/modules/app_service/azurerm_service_plan.tf
@@ -1,7 +1,7 @@
 # App Service Plan
 resource "azurerm_service_plan" "app_plan" {
-  name                = "securedemo-plan"
-  location            = "japanwest"
+  name                = "securedemo-plan${random_string.main.result}"
+  location            = var.resource_group.location
   resource_group_name = var.resource_group.name
   os_type             = "Linux"
   sku_name            = "S1" # Changed from P1v2 to S1 due to quota limitations
@@ -9,4 +9,11 @@ resource "azurerm_service_plan" "app_plan" {
     environment = "dev"
     purpose     = "secure-webapp"
   }
+}
+
+resource "random_string" "main" {
+  length  = 16
+  special = false
+  upper   = false
+
 }
